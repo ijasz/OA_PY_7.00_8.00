00b3d6f82d1f967ccebb716de4650244c28962be
“ijasz” <jass07rtr@gmail.com>
2023-05-29 19:28:58 +0530

    first commit

---
+++ _operator.py
@@ -0,0 +1,25 @@
+# Operator
+
+# print(4 + 5)  # add
+# print(4 - 5)  # sub
+# print(5 / 2)  # div
+# print(5 // 2)  # floor div
+# print(5 % 2)  # mod
+# print(2 ** 2)  # ex
+# print(4 * 5)  # mul
+
+# Assignment operator
+# += -= /= //= %= **= *=
+
+# a = 1
+
+# a = a + 1
+
+# a *= 2
+# a += 2
+
+# a = a * 2
+# print(a)
+
+# a = a + 2
+# print(a)
