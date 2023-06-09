8e5689a97cac66fef61bf11bad9a525da38e5ccc
“ijasz” <jass07rtr@gmail.com>
2023-06-09 20:13:20 +0530

    for loop and task updated

---
+++ loop-task.py
@@ -0,0 +1,64 @@
+# Task1
+# case1
+
+# input
+# what table you want? => 2
+# How many times you wantv this table? => 2
+
+# output
+# 2 * 1 = 2
+# 2 * 2 = 4
+
+# case2
+
+# input
+# what table you want? => 5
+# How many times you want this table? => 5
+
+# output
+# 5 * 1 = 5
+# 5 * 2 = 10
+# 5 * 3 = 15
+# 5 * 4 = 20
+# 5 * 5 = 25
+
+# ----------------------------------------
+
+# Task1
+# Matrix
+
+# case1
+
+# input => 2
+# what to be print for matrix format => ☻
+
+# output
+# ☻ ☻
+# ☻ ☻
+# matrix - 2 * 2
+
+# case2
+
+# input => 4
+# what to be print for matrix format => ♦
+
+# output
+# ♦ ♦ ♦ ♦
+# ♦ ♦ ♦ ♦
+# ♦ ♦ ♦ ♦
+# ♦ ♦ ♦ ♦
+# matrix - 4 * 4
+
+# ----------------------------------------
+
+# Task3
+
+# input 3
+
+# output
+
+# 1
+# 1 1
+# 1 1 1
+
+# ----------------------------------------
